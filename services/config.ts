c7ef7ae8528ae01f6c463f0d5b7ac7acf8f50bd1
Gerard Sole <58429274+Gerardss95@users.noreply.github.com>
2023-09-01 00:00:53 +0200

    added oneUserPosts page + env

--- services/config.ts
+++ services/config.ts
@@ -4,13 +4,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyAKiwAJSSKDyr4SEQ3Hwf7kMnO9BmGdki0',
-  authDomain: 'geekpost-13b45.firebaseapp.com',
-  projectId: 'geekpost-13b45',
-  storageBucket: 'geekpost-13b45.appspot.com',
-  messagingSenderId: '438533025259',
-  appId: '1:438533025259:web:1ff971e9e399c71222bdae',
-  measurementId: 'G-0FZJJLJ7RJ'
+  apiKey: process.env.EXPO_PUBLIC_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
@@ -18,4 +18,4 @@ const db = getFirestore(app);
 const auth = getAuth(app);
 const storage = getStorage(app);
 
-export { app, db, auth, storage};
+export { app, db, auth, storage };
